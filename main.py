d08318aeaa61d09651d2d31df02f156ce6c2b768
callum <callumfrance@users.noreply.github.com>
2018-08-15 21:47:45 +0800

    better exit handling and print statements

--- main.py
+++ main.py
@@ -7,21 +7,24 @@ toExit = False
 while not toExit:
     # Clears the screen before printing again
     os.system('cls' if os.name == 'nt' else 'clear')
+    write_filename = ''
 
-    print("Enter name of file to be converted to java file:")
+    print("Enter name of file to be converted gridded into a java / python file:")
     read_filename = input("> ")
 
-    in_grid = read_file(read_filename)
-    print("File has been read")
-    print(in_grid)
+    if read_filename != "exit":
+        in_grid = read_file(read_filename)
+        print("File has been read")
+        print(in_grid)
 
 
-    print("Enter name of java file:")
-    write_filename = input("> ")
+        print("Enter name of java file:")
+        write_filename = input("> ")
 
-    write_java_file(write_filename, in_grid)
-    write_python_file(write_filename, in_grid)
-    print("File has been written")
+    if write_filename != "exit":
+        write_java_file(write_filename, in_grid)
+        write_python_file(write_filename, in_grid)
+        print("File has been written")
 
     if write_filename == "exit" or read_filename == "exit":
         toExit = True
